3e65a02cd88cc64690e787e18886a44af75f1e9e
reachcool <reachcool@163.com>
2024-05-07 23:12:21 +0800

    vnode 4th

--- MCTP vNode.sol
+++ MCTP vNode.sol
@@ -32,7 +32,7 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     bool public halted = false;
 
     
-      // Optional mapping for token URIs
+    // Optional mapping for token URIs
     //mapping (uint256 => string) private _tokenURIs;
     // address's parent
     mapping (address=>address) public parent;
@@ -40,8 +40,8 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     mapping (uint256=>address) public tokenOwner;
     mapping (address => NodeInfo) public nodes;
 
-    uint256 constant public PRICE_INCREMENT = 500;
-    uint256 constant public ID_INCREMENT = 5;
+    uint256 constant public PRICE_INCREMENT = 500;//500
+    uint256 constant public ID_INCREMENT = 5; //500
     uint256 constant public MAX_ID = 50000;
 
     address public publicKey = address(0xEe8b45a0c599e8E6512297f99687BF5FE3359147);
@@ -49,8 +49,8 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     // Event emitted when an NFT is minted
 
     event NodeStatusChange(address owner, address parent, uint256  tokenId, string status, uint256 timestamp);
-    event TransactionCreated(address withdrawAddress,  uint256 timestamp, uint256 transactionId);
-    event TransactionCompleted(address withdrawAddress,  uint256 timestamp, uint256 transactionId);
+    //event TransactionCreated(address withdrawAddress,  uint256 timestamp, uint256 transactionId);
+    //event TransactionCompleted(address withdrawAddress,  uint256 timestamp, uint256 transactionId);
     event TransactionSigned(address by, uint transactionId);
 
     event NodeEvents(
@@ -58,21 +58,6 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
         address eventSender,          
         string  eventName
     );
-    event TransactionReadyForExecution(
-        address itemToken,
-        uint256 timestamp,
-        uint transactionId
-    );
-
-    struct Transaction {
-      address withdrawAddress_;
-      bool locked_;
-      uint256 nodeCount_;
-      uint256 mintPrice_;
-      uint8 signatureCount;
-      uint256 readyForExecutionTimestamp; 
-      uint256 timestamp;
-    }
 
     struct TokenInfo {
         uint256 tokenId;
@@ -88,14 +73,15 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     enum TransactionType {
         AddOwner,
         RemoveOwner,
-        SetParameter
-        //SetSalesAddress
+        SetParameter,
+        SetSalesAddress
     }
     struct SetTransaction {
         TransactionType txType;
         bytes data;
         uint8 signatureCount;
         uint256 timestamp;
+        uint256 readyForExecutionTimestamp; 
         bool executed;
     }
 
@@ -115,10 +101,10 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
 
     mapping(address => uint8) private _validOwners;
     //mapping ( uint=>mapping(address => uint8)) signatures;
-    mapping (uint => Transaction) private _transactions;
+    //mapping (uint => Transaction) private _transactions;
     //uint[] private _pendingTransactions;
-    uint256 public constant observationPeriod = 24 ;
-    uint256 public constant maxPendingTime = 96 ;
+    uint256 public constant observationPeriod = 24 hours; //hours
+    uint256 public constant maxPendingTime = 96 hours; // hours
 
     uint constant MIN_SIGNATURES = 3;
     uint private _transactionIdx;
@@ -159,95 +145,13 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
         require(_validOwners[msg.sender] == 1, "not Authorized Mulsig User !");
         _;
     }
-   function isValidFunctionSelector(TransactionType _type, bytes4 selector) private pure returns (bool) {
-    if (_type == TransactionType.AddOwner && selector == this.setOwner.selector) return true;
-    if (_type == TransactionType.RemoveOwner && selector == this.setOwner.selector) return true;
-    if (_type == TransactionType.SetParameter && selector == this.setLockHalt.selector) return true;
-    //if (_type == TransactionType.SetSalesAddress && selector == this.setSalesAddress.selector) return true;
-    return false;
-}
-    function  deleteMaxPendingTx()
-
-    private 
-    {
-         require(_pendingTransactions.length <= 1,"has pending txs! Please sign them first");   
-         
-         if(_pendingTransactions.length == 1 ){
-                uint txId = _pendingTransactions[0];
-                Transaction storage pendingwithdrawTx = _transactions[txId];
-               
-                SetTransaction storage pendingsetTx = _settransactions[txId];
-                //for withdraw Txs
-                if (pendingwithdrawTx.timestamp > 0 ){
-                    if(block.timestamp - pendingwithdrawTx.timestamp > maxPendingTime){
-                         deleteTransaction(txId);
-                    } else revert("has pending txs yet! Please sign them first");
-                   
-                }
-               
-                //for set param Txs
-                if (pendingsetTx.timestamp > 0 ){
-                    if (block.timestamp - pendingsetTx.timestamp > maxPendingTime){
-                        deleteSetTx(txId);
-                    }else revert("has pending txs yet! Please sign them first"); 
-                }
-
-            }
-    }
-    function setOwner(address _owner, uint8 _status) public onlySelf {
-        _validOwners[_owner] = _status;
-    }
-    function setLockHalt(bool isLock_, bool isHalt_ ) public onlySelf{
-        locked=isLock_;
-        halted = isHalt_;
-    }
-
-    function addSetTransaction(TransactionType _type, bytes memory _data) public {
-        require(msg.sender==owner,"Only owner can set Parameters");
-        require(_data.length >= 4, "Data too short for a valid function selector");
-        require(isValidFunctionSelector(_type, bytes4(_data)), "Invalid function selector for transaction type");
-
-        uint256 transactionId = _transactionIdx++;
-
-        _settransactions[transactionId] = SetTransaction({
-            txType: _type,
-            data: _data,
-            signatureCount: 0,
-            timestamp: block.timestamp,
-            executed: false
-        });
-        _pendingTransactions.push(transactionId);
-        
-    }
-    // Function for authorized owners to sign pending transactions
-    function signSetTransaction(uint transactionId) public validOwner notExecuted(transactionId) {
-        SetTransaction memory transaction = _settransactions[transactionId];
-        require(transaction.timestamp > 0, "transaction not exist!");
-        require(!transaction.executed, "Transaction already executed");
-
-        require(!signatures[transactionId][msg.sender], "Signature already provided");
-
-        signatures[transactionId][msg.sender] = true;
-        _settransactions[transactionId].signatureCount++;
-        _settransactions[transactionId].timestamp = block.timestamp;
-        
-        if (_settransactions[transactionId].signatureCount == MIN_SIGNATURES) {
-            (bool success,) = address(this).call(_settransactions[transactionId].data);
-            require(success, "Transaction execution failed");
-
-            _settransactions[transactionId].executed = true;
-            deleteSetTx(transactionId);
-        }
-
-    }
 
     function addOwner(address _owner)
         public 
         {
             require(msg.sender==owner,"Only owner can set Parameters");
             require(_owner != address(0),"Zero Address Error!");
-            deleteMaxPendingTx();
-
+           
             TransactionType txType = TransactionType.AddOwner;
             bytes memory data = abi.encodeWithSelector(this.setOwner.selector, _owner, 1);
             addSetTransaction(txType, data);
@@ -259,7 +163,7 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
         public {
             require(msg.sender==owner,"Only owner can set Parameters");
             require(_owner != address(0),"Zero Address Error!");
-            deleteMaxPendingTx();
+            
             TransactionType txType = TransactionType.RemoveOwner;
             bytes memory data = abi.encodeWithSelector(this.setOwner.selector, _owner, 0);
             addSetTransaction(txType, data);
@@ -274,7 +178,7 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     {
         require(msg.sender==owner,"Only owner can set Parameters");
        
-        deleteMaxPendingTx();
+    
         TransactionType txType = TransactionType.SetParameter;
         bytes memory data = abi.encodeWithSelector(this.setLockHalt.selector, isLock_, isHalt_);
         addSetTransaction(txType, data);
@@ -282,7 +186,123 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
         emit NodeEvents(block.timestamp,msg.sender, "setParamters");
     }
 
-   
+    function setSalesAddress ( address _feeAddress)
+    public
+    {
+        require(msg.sender==owner,"Only owner can set Parameters");
+       
+        //deleteMaxPendingTx();
+        TransactionType txType = TransactionType.SetSalesAddress;
+        bytes memory data = abi.encodeWithSelector(this.setFeeAddress.selector, _feeAddress);
+        addSetTransaction(txType, data);
+
+        emit NodeEvents(block.timestamp,msg.sender, "setSalesAddress");
+    }
+
+   function isValidFunctionSelector(TransactionType _type, bytes4 selector) private pure returns (bool) {
+        if (_type == TransactionType.AddOwner && selector == this.setOwner.selector) return true;
+        if (_type == TransactionType.RemoveOwner && selector == this.setOwner.selector) return true;
+        if (_type == TransactionType.SetParameter && selector == this.setLockHalt.selector) return true;
+        if (_type == TransactionType.SetSalesAddress && selector == this.setFeeAddress.selector) return true;
+        return false;
+    }
+    function  deleteMaxPendingTx()
+
+    private 
+    {
+         require(_pendingTransactions.length <= 1,"has pending txs! Please sign them first");   
+         
+         if(_pendingTransactions.length == 1 ){
+                uint txId = _pendingTransactions[0];
+                
+                SetTransaction storage pendingsetTx = _settransactions[txId];
+                               
+                //for set param Txs
+                if (pendingsetTx.timestamp > 0 ){
+                    if (block.timestamp - pendingsetTx.timestamp > maxPendingTime){
+                        deleteSetTx(txId);
+                    }else revert("has pending txs yet! Please sign them first"); 
+                }
+
+            }
+    }
+
+    function setOwner(address _owner, uint8 _status) public onlySelf {
+        _validOwners[_owner] = _status;
+    }
+    function setLockHalt(bool isLock_, bool isHalt_ ) public onlySelf{
+        locked=isLock_;
+        halted = isHalt_;
+    }
+    function setFeeAddress(address _feeAddress) public onlySelf{
+        feeAddress = payable(_feeAddress);
+    }
+
+    function addSetTransaction(TransactionType _type, bytes memory _data) private {
+        require(msg.sender == owner,"Only owner can set Parameters");
+        require(_data.length >= 4, "Data too short for a valid function selector");
+        require(isValidFunctionSelector(_type, bytes4(_data)), "Invalid function selector for transaction type");
+        
+        deleteMaxPendingTx();
+
+        uint256 transactionId = _transactionIdx++;
+
+        _settransactions[transactionId] = SetTransaction({
+            txType: _type,
+            data: _data,
+            signatureCount: 0,
+            timestamp: block.timestamp,
+            readyForExecutionTimestamp:block.timestamp,
+            executed: false
+        });
+        _pendingTransactions.push(transactionId);
+        
+    }
+    // Function for authorized owners to sign pending transactions
+    function signSetTransaction(uint transactionId) public validOwner notExecuted(transactionId) {
+        SetTransaction memory transaction = _settransactions[transactionId];
+        require(transaction.timestamp > 0, "transaction not exist!");
+        require(!transaction.executed, "Transaction already executed");
+
+        require(!signatures[transactionId][msg.sender], "Signature already provided");
+
+        if (transaction.txType == TransactionType.SetSalesAddress){
+           require(block.timestamp - transaction.timestamp >= 24 hours,"Time Lockin for 24 Hours for each signers !"); //hours
+        }
+
+        signatures[transactionId][msg.sender] = true;
+        _settransactions[transactionId].signatureCount++;
+        _settransactions[transactionId].timestamp = block.timestamp;
+        
+        if (_settransactions[transactionId].signatureCount == MIN_SIGNATURES) {
+                if (transaction.txType == TransactionType.SetSalesAddress){
+              _settransactions[transactionId].readyForExecutionTimestamp = block.timestamp + observationPeriod;  
+           }
+           else{
+              _settransactions[transactionId].readyForExecutionTimestamp = block.timestamp;
+              executeSetTransaction(transactionId);
+           }
+            
+        }
+        emit TransactionSigned(msg.sender, transactionId);
+
+    }
+
+   function executeSetTransaction(uint transactionId) 
+    validOwner
+    public  {
+        SetTransaction storage transaction = _settransactions[transactionId];
+        require(transaction.signatureCount >= MIN_SIGNATURES,"Signs Not Satisfied");
+        require(transaction.readyForExecutionTimestamp>0,"Transaction is not ready for execution");
+        require(block.timestamp >= transaction.readyForExecutionTimestamp, "Transaction is not ready for execution");
+
+        (bool success,) = address(this).call(transaction.data);
+        require(success, "Transaction execution failed");
+
+        transaction.executed = true;
+        deleteSetTx(transactionId);
+    }
+
     function deleteSetTx(uint transactionId)
         
         private {
@@ -311,100 +331,19 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     function getPendingTransaction(uint transactionId)
       view
       public
-      returns (Transaction memory, SetTransaction memory) {
-        Transaction storage pendingwithdrawTx = _transactions[transactionId];
-        SetTransaction storage pendingsetTx = _settransactions[transactionId];
-        return (pendingwithdrawTx,pendingsetTx);
+      returns ( SetTransaction memory) {
+        //Transaction storage pendingwithdrawTx = _transactions[transactionId];
+        SetTransaction memory pendingsetTx = _settransactions[transactionId];
+        return pendingsetTx;
     }
     function  getValidOwner(address _owner)  view public returns (uint){
-       
+
         return _validOwners[_owner];
     }
-     function signTransaction(uint transactionId) 
-     validOwner 
-     public {
-        Transaction storage transaction = _transactions[transactionId];
-       
-        require(address(0) != transaction.withdrawAddress_,  "Fee To Zero Addresses!");
-        // Creator cannot sign the transaction
-        require(msg.sender !=transaction.withdrawAddress_ , "Can't Sign Self!" );
-        // Cannot sign a transaction more than once
-        require(!signatures[transactionId][msg.sender], "Can't Sign Again with the same Account!");
-        // no more than MIN_SIGNATURES Signs
-        require(transaction.signatureCount < MIN_SIGNATURES, "MS has satisfied, No need further Sign!");
-        // can not sign within once within 24 hours
-        require(block.timestamp - transaction.timestamp >= 24,"Time Lockin for 24 Hours for each signers !");
 
-        signatures[transactionId][msg.sender] = true;
-
-        transaction.signatureCount++;
-        transaction.timestamp= block.timestamp;
-        
-        if (transaction.signatureCount == MIN_SIGNATURES) {
-            transaction.readyForExecutionTimestamp = block.timestamp + observationPeriod;
-            emit TransactionReadyForExecution(transaction.withdrawAddress_, transaction.readyForExecutionTimestamp, transactionId);
-        }
-    }
-
-    function executeTransaction(uint transactionId) 
-    validOwner
-    public  {
-        Transaction storage transaction = _transactions[transactionId];
-        require(transaction.signatureCount >= MIN_SIGNATURES,"Signs Not Satisfied");
-        require(transaction.readyForExecutionTimestamp>0,"Transaction is not ready for execution");
-        require(block.timestamp >= transaction.readyForExecutionTimestamp, "Transaction is not ready for execution");
-
-        feeAddress = payable(transaction.withdrawAddress_);
-        emit TransactionCompleted(transaction.withdrawAddress_, block.timestamp, transactionId);
-        deleteTransaction(transactionId);
-    }
-
-    function deleteTransaction(uint transactionId)
-        
-        private 
-        {
-            Transaction memory transaction = _transactions[transactionId];
-            require(transaction.timestamp > 0, "transaction not exist!");
-           
-            uint256 txLength = _pendingTransactions.length;
-            for (uint256 i = 0; i < txLength; i++) {
-                if (_pendingTransactions[i] == transactionId) {
-                    
-                    _pendingTransactions[i] = _pendingTransactions[txLength - 1];
-                   
-                    _pendingTransactions.pop();
-                    break;
-                }
-            }
-            delete _transactions[transactionId];
-    }
-
-    function setSalesAddress ( address _withdrawAddress)
-    public
     
-    {
-        require(msg.sender==owner,"Only owner can set Parameters");
-        require(_withdrawAddress != address(0),"Zero Address Error!");
+    /*****************************************************/
 
-        deleteMaxPendingTx();
-
-       
-        uint256 transactionId = _transactionIdx++;
-
-        Transaction memory transaction;
-        
-        transaction.withdrawAddress_ = _withdrawAddress;
-
-        transaction.timestamp = block.timestamp;
-        transaction.signatureCount = 0;
-        _transactions[transactionId]=transaction;
-        _pendingTransactions.push(transactionId);
-
-        emit TransactionCreated(_withdrawAddress,  block.timestamp, transactionId);
-        emit NodeEvents(block.timestamp,msg.sender, "setFeeAddress");
-    }
-    
-    
 
     function tokenURI(uint256 tokenId) public view virtual 
     override(ERC721) 
@@ -454,7 +393,7 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
         ERC20Token.safeTransferFrom(msg.sender, feeAddress, mintPrice);
 
         _mint(address(this), newItemId);
-
+        
         tokenOwner[newItemId] = msg.sender;
 
         _mintedAddresses[msg.sender]=true;
@@ -584,13 +523,12 @@ contract MetaCraftVNode is ERC721, IERC721Receiver,ReentrancyGuard{
     }
 
     function calculatePrice(uint256 id) public view   returns (uint256) {
-        if (id > MAX_ID) {
-            revert("ID exceeds maximum limit");
-        }
+      
+        require(id<= MAX_ID,"ID exceeds maximum limit");
 
         uint256 priceLevel = ((id-1) / ID_INCREMENT) + 1;
-        return priceLevel * PRICE_INCREMENT * 10** tokenDecimals;
+        return priceLevel * PRICE_INCREMENT * 10** tokenDecimals ;
+       
     }
-
-      
+       
 }
